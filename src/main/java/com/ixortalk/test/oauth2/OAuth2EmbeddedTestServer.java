cf0d8b58cce366db4d2a06fe2ca71f026527801d
Ward Jans <ward.jans@wja-consulting.be>
2019-09-30 17:30:24 +0200

    Refactoring out Auth0 dependency (not used anymore in integration tests since the introduction of ixortalk-oauth2-spring-boot-starter)

--- src/main/java/com/ixortalk/test/oauth2/OAuth2EmbeddedTestServer.java
+++ src/main/java/com/ixortalk/test/oauth2/OAuth2EmbeddedTestServer.java
@@ -24,9 +24,7 @@
 package com.ixortalk.test.oauth2;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -36,10 +34,6 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import static org.springframework.core.Ordered.LOWEST_PRECEDENCE;
 
@@ -67,30 +61,7 @@ public class OAuth2EmbeddedTestServer {
 
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-            endpoints.tokenStore(tokenStore())
-                    .accessTokenConverter(accessTokenConverter())
-                    .authenticationManager(authenticationManager);
-        }
-
-        @Bean
-        public TokenStore tokenStore() {
-            return new JwtTokenStore(accessTokenConverter());
-        }
-
-        @Bean
-        public JwtAccessTokenConverter accessTokenConverter() {
-            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-            converter.setSigningKey("testSigningKey");
-            return converter;
-        }
-
-        @Bean
-        @Primary
-        public DefaultTokenServices tokenServices() {
-            DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-            defaultTokenServices.setTokenStore(tokenStore());
-            defaultTokenServices.setSupportRefreshToken(true);
-            return defaultTokenServices;
+            endpoints.authenticationManager(authenticationManager);
         }
 
         @Override
